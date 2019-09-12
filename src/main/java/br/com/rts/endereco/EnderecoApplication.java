6fd32044b3a95f36c3de2910f8dabf38ce683e92
Rodolpho T. Sotolani <rt.sotolani@gmail.com>
2019-09-11 23:39:33 -0300

    Initial commit

--- src/main/java/br/com/rts/endereco/EnderecoApplication.java
+++ src/main/java/br/com/rts/endereco/EnderecoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EnderecoDemoApplication {
+public class EnderecoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EnderecoDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EnderecoApplication.class, args);
+    }
 
 }
--- src/test/java/br/com/rts/endereco/EnderecoApplicationTests.java
+++ src/test/java/br/com/rts/endereco/EnderecoApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.sotolani.endereco;
+package br.com.rts.endereco;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class EnderecoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
